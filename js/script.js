6d452a3df3c61449e66ccbeff7a73bcaddc2dee5
cesar_v <indigo.peacewaters@gmail.com>
2025-12-27 20:40:46 -0600

    Add JavaScript click event for a cat appearance

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 var button = document.querySelector(".show-cat");
 // console.log(button.outerHTML);
 var cat = document.querySelector(".cat");
-console.log(cat.outerHTML);
+// console.log(cat.outerHTML);
+
+//Clicking on the button reveals a cat image, by adding the ".cat" class with an opacity style of 1.
+button.addEventListener("click", function () {
+  button.innerText = "Meow!";
+  cat.classList.add("show");
+});
